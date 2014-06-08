0d082c6c978b442788ba00a3911509380e7d3514
DK <dukai86@gmail.com>
2014-06-09 00:35:12 +0800

    更新拍照

--- src/net/yeelink/yeelinkiotclient/CameraCapture.java
+++ src/net/yeelink/yeelinkiotclient/CameraCapture.java
@@ -2,15 +2,29 @@ package net.yeelink.yeelinkiotclient;
 
 import java.io.IOException;
 
+import net.yeelink.sdk.HttpClient;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.hardware.Camera.AutoFocusCallback;
 import android.hardware.Camera.Parameters;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
 import android.os.Build;
@@ -22,6 +36,24 @@ public class CameraCapture extends Activity {
 	private boolean run = true;
 	public Button btnFocus;
 	public Button btnTake;
+	public ImageView ivPreview;
+	
+	public Handler handler = new Handler(Looper.getMainLooper()) {
+		@Override
+		public void handleMessage(Message inputMessage) {
+			switch (inputMessage.what) {
+				case 0:
+					break;
+				case 1:
+					byte[] data = (byte[]) inputMessage.obj;
+					Bitmap bm = BitmapFactory.decodeByteArray(data, 0, data.length);
+					ivPreview.setImageBitmap(bm);
+					break;
+			}
+		}
+
+	};
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +61,72 @@ public class CameraCapture extends Activity {
 		setContentView(R.layout.activity_camera_capture);
 		// Show the Up button in the action bar.
 		setupActionBar();
-		
+
 		svCamera = (SurfaceView) findViewById(R.id.sv_camera);
 		svCamera.getHolder().addCallback(new SurfaceCallback());
 		svCamera.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-		
+
 		btnFocus = (Button) findViewById(R.id.btn_focus);
 		btnTake = (Button) findViewById(R.id.btn_take);
 		
+		ivPreview = (ImageView) findViewById(R.id.iv_preview);
+
+		btnFocus.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (camera != null) {
+					camera.autoFocus(new AutoFocusCallback() {
+
+						@Override
+						public void onAutoFocus(boolean success,
+								Camera camera) {
+							// TODO Auto-generated method stub
+
+						}
+
+					});
+				}
+			}
+
+		});
+
+		btnTake.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (camera != null) {
+					camera.takePicture(new ShutterCallback() {
+						public void onShutter() {
+						}
+					}, null, new PictureCallback() {
+
+						@Override
+						public void onPictureTaken(byte[] data,
+								Camera arg1) {
+							Message completeMessage = handler.obtainMessage(1,
+									data);
+							completeMessage.sendToTarget();
+							HttpClient http = new HttpClient();
+							http.addHeader("U-APIKEY", "4d0cd8e2e9cd21714b10696f80645d42");
+							http.post("http://api.yeelink.net/v1.0/device/10879/sensor/18281/photos", data, null);
+						}
+
+					});
+				}
+			}
+
+		});
+
 	}
-	
-	private class SurfaceCallback implements SurfaceHolder.Callback{
+
+	private class SurfaceCallback implements SurfaceHolder.Callback {
 
 		@Override
 		public void surfaceChanged(SurfaceHolder holder, int format, int width,
 				int height) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
@@ -53,14 +134,14 @@ public class CameraCapture extends Activity {
 			// TODO Auto-generated method stub
 			try {
 				camera = Camera.open();// 打开摄像头
-				camera.setDisplayOrientation(90);
+				//camera.setDisplayOrientation(90);
 				camera.setPreviewDisplay(svCamera.getHolder());// 设置picSV来进行预览取景
-				
+
 				Parameters params = camera.getParameters();// 获取照相机的参数
-				params.setPictureSize(480, 320);// 设置照片的大小为800*480
-				params.setPreviewSize(480, 320);// 设置预览取景的大小为800*480
-				params.setFlashMode(Parameters.FLASH_MODE_ON);// 开启闪光灯
-				params.setJpegQuality(60);// 设置图片质量为50
+				params.setPictureSize(800, 480);// 设置照片的大小为800*480
+				//params.setPreviewSize(480, 800);// 设置预览取景的大小为800*480
+				params.setFlashMode(Parameters.FLASH_MODE_OFF);// 开启闪光灯
+				params.setJpegQuality(70);// 设置图片质量为50
 
 				camera.setParameters(params);// 设置以上参数为照相机的参数
 				camera.startPreview();
@@ -76,7 +157,7 @@ public class CameraCapture extends Activity {
 			camera.release();
 			camera = null;
 		}
-		
+
 	}
 
 	/**
